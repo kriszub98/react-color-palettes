eb6fa0b3f58dbdedbfa5511db10b79c4d14e1995
Krzysztof Zubrzycki <kriszub98@gmail.com>
2022-06-21 14:01:07 +0200

    Add React Router

--- colors-app/src/App.js
+++ colors-app/src/App.js
@@ -1,14 +1,21 @@
 import Palette from './Palette';
+import { Route, Routes } from 'react-router-dom';
 import seedColors from './seedColors';
 import { generatePalette } from './colorHelpers';
 import './App.css';
 
 function App() {
 	return (
-		<div className="App">
-			<Palette palette={generatePalette(seedColors[4])} />
-		</div>
+		<Routes>
+			<Route exact path="/" element={<h1>Palette</h1>} />
+			<Route exact path="/palette/:id" element={<h1>Indiv Route</h1>} />
+		</Routes>
 	);
+	{
+		/* <div className="App">
+		<Palette palette={generatePalette(seedColors[4])} />
+	</div> */
+	}
 }
 
 export default App;
