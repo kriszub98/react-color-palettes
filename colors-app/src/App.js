e62ad928f5144b3b805d3a8a4973e380ab9e3721
Krzysztof Zubrzycki <kriszub98@gmail.com>
2022-06-21 21:42:21 +0200

    Add PaletteList and Links

--- colors-app/src/App.js
+++ colors-app/src/App.js
@@ -1,19 +1,16 @@
 import Palette from './Palette';
+import PaletteList from './PaletteList';
 import { Route, Routes } from 'react-router-dom';
+import seedColors from './seedColors';
 import './App.css';
 
 function App() {
 	return (
 		<Routes>
-			<Route exact path="/" element={<h1>Palette</h1>} />
+			<Route exact path="/" element={<PaletteList palettes={seedColors} />} />
 			<Route exact path="/palette/:id" element={<Palette />} />
 		</Routes>
 	);
-	{
-		/* <div className="App">
-		<Palette palette={generatePalette(seedColors[4])} />
-	</div> */
-	}
 }
 
 export default App;
