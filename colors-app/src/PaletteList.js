c888a7321ac9eab3345efcb621be4c0bdc04c491
Krzysztof Zubrzycki <kriszub98@gmail.com>
2022-06-28 15:30:06 +0200

    Start styling PaletteList

--- colors-app/src/PaletteList.js
+++ colors-app/src/PaletteList.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import MiniPalette from './MiniPalette';
+import './PaletteList.css';
 
 const PaletteList = ({ palettes }) => {
 	return (
-		<div>
-			<h1>React Colors</h1>
-			{palettes.map((palette) => <MiniPalette key={palette.id} {...palette} />)}
+		<div className="PaletteList-root">
+			<div className="PaletteList-container">
+				<nav className="PaletteList-nav">
+					<h1>React Colors</h1>
+				</nav>
+				<div className="PaletteList-palettes">
+					{palettes.map((palette) => <MiniPalette key={palette.id} {...palette} />)}
+				</div>
+			</div>
 		</div>
 	);
 };
