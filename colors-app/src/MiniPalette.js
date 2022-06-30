4f35faf2c3105326f9536e9593a465b5edc001b9
Krzysztof Zubrzycki <kriszub98@gmail.com>
2022-06-30 22:59:41 +0200

    Add Link to palette

--- colors-app/src/MiniPalette.js
+++ colors-app/src/MiniPalette.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './MiniPalette.css';
 
-const MiniPalette = ({ paletteName, emoji, colors }) => {
+const MiniPalette = ({ paletteName, emoji, colors, handleClick }) => {
 	const miniColorBoxes = colors.map((color) => (
 		<div key={color.name} className="MiniPalette-miniColor" style={{ backgroundColor: color.color }} />
 	));
 
 	return (
-		<div className="MiniPalette-root">
+		<div className="MiniPalette-root" onClick={handleClick}>
 			<div className="MiniPalette-colors">{miniColorBoxes}</div>
 
 			<h5 className="MiniPalette-title">
--- colors-app/src/PaletteList.js
+++ colors-app/src/PaletteList.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import MiniPalette from './MiniPalette';
 import './PaletteList.css';
 
 const PaletteList = ({ palettes }) => {
+	const navigation = useNavigate();
+
+	const goToPalette = (id) => {
+		return navigation(`/palette/${id}`);
+	};
+
 	return (
 		<div className="PaletteList-root">
 			<div className="PaletteList-container">
@@ -10,7 +17,9 @@ const PaletteList = ({ palettes }) => {
 					<h1>React Colors</h1>
 				</nav>
 				<div className="PaletteList-palettes">
-					{palettes.map((palette) => <MiniPalette key={palette.id} {...palette} />)}
+					{palettes.map((palette) => (
+						<MiniPalette key={palette.id} handleClick={() => goToPalette(palette.id)} {...palette} />
+					))}
 				</div>
 			</div>
 		</div>
