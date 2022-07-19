f3067d95b1e137c35d82dd827bb5b5ab28329b46
Krzysztof Zubrzycki <kriszub98@gmail.com>
2022-07-19 17:02:04 +0200

    Add Link to More Button inside ColorBox and add route

--- colors-app/src/App.js
+++ colors-app/src/App.js
@@ -9,6 +9,7 @@ function App() {
 		<Routes>
 			<Route exact path="/" element={<PaletteList palettes={seedColors} />} />
 			<Route exact path="/palette/:id" element={<Palette />} />
+			<Route exact path="/palette/:paletteId/:colorId" element={<h1>Single Color</h1>} />
 		</Routes>
 	);
 }
