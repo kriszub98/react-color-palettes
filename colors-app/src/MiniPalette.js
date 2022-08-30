c2a1853b624849f8fab1a802de6e403349404b36
Krzysztof Zubrzycki <kriszub98@gmail.com>
2022-08-30 19:50:28 +0200

    MiniPalette Delete styling fix

--- colors-app/src/MiniPalette.js
+++ colors-app/src/MiniPalette.js
@@ -14,12 +14,8 @@ const MiniPalette = ({ paletteName, emoji, colors, handleClick, deletePalette })
 
 	return (
 		<div className="MiniPalette-root" onClick={handleClick}>
-			<div className="MiniPalette-delete">
-				<DeleteIcon
-					className="MiniPalette-deleteIcon"
-					style={{ transition: 'all 0.3s ease-in-out' }}
-					onClick={handleDeletePaletteClick}
-				/>
+			<div className="MiniPalette-delete" style={{ transition: 'all 0.3s ease-in-out' }}>
+				<DeleteIcon className="MiniPalette-deleteIcon" onClick={handleDeletePaletteClick} />
 			</div>
 
 			<div className="MiniPalette-colors">{miniColorBoxes}</div>
