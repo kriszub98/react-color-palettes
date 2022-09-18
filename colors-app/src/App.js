f0bee2f2bab786f7da5c12b942064e1edb3bd092
Krzysztof Zubrzycki <kriszub98@gmail.com>
2022-09-18 20:11:35 +0200

    Add basic route transitions

--- colors-app/src/App.js
+++ colors-app/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useLocation } from 'react-router-dom';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 import Palette from './Palette';
 import PaletteList from './PaletteList';
 import seedColors from './seedColors';
@@ -11,6 +12,7 @@ function App() {
 	const savedPalettes = JSON.parse(window.localStorage.getItem('palettes')) || seedColors;
 	const [ palettes, setPalettes ] = React.useState(savedPalettes);
 	const didMount = React.useRef(false);
+	const location = useLocation();
 
 	React.useEffect(
 		() => {
@@ -40,20 +42,30 @@ function App() {
 	};
 
 	return (
-		<Routes>
-			<Route exact path="/" element={<PaletteList palettes={palettes} deletePalette={deletePalette} />} />
-			<Route
-				exact
-				path="/palette/new"
-				element={<NewPaletteForm savePalette={savePalette} palettes={palettes} />}
-			/>
-			<Route exact path="/palette/:id" element={<Palette findPalette={findPalette} />} />
-			<Route
-				exact
-				path="/palette/:paletteId/:colorId"
-				element={<SingleColorPalette findPalette={findPalette} />}
-			/>
-		</Routes>
+		<div className="root">
+			<TransitionGroup>
+				<CSSTransition key={location.pathname} classNames="fade" timeout={400}>
+					<Routes>
+						<Route
+							exact
+							path="/"
+							element={<PaletteList palettes={palettes} deletePalette={deletePalette} />}
+						/>
+						<Route
+							exact
+							path="/palette/new"
+							element={<NewPaletteForm savePalette={savePalette} palettes={palettes} />}
+						/>
+						<Route exact path="/palette/:id" element={<Palette findPalette={findPalette} />} />
+						<Route
+							exact
+							path="/palette/:paletteId/:colorId"
+							element={<SingleColorPalette findPalette={findPalette} />}
+						/>
+					</Routes>
+				</CSSTransition>
+			</TransitionGroup>
+		</div>
 	);
 }
 
